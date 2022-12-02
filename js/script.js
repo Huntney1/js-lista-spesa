eb2fa7504e5b0ef138f3aa5d99231e31594307dd
Huntney1 <devilwhiteone@hotmail.it>
2022-12-02 15:24:55 +0100

    fine esercizio

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const list = [
+    'Cioccolata',
+    'Guanciale',
+    'Pecorino',
+    'Pasta',
+    'Uova',
+    'Pasta di pomodoro',
+    'Piatti',
+    'Acqua',
+]
+
+const list_container = document.querySelector('.shopping-list');
+
+let element = '';
+let item = 0;
+
+while( item < list.length){
+
+   let list_item = document.createElement('li')
+
+    list_item.innerText = list[item];
+
+    list_item.classList.add('list');
+
+    list_container.append(list_item)
+    item++
+}
